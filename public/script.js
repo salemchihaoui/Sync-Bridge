569e7681aa08cae30525d0a8a4b15f0b009d19f0
salemchihaoui <salemchihaoui2001@gmail.com>
2025-01-22 16:29:49 +0100

    feat: enhance UX and add dark mode support

--- public/script.js
+++ public/script.js
@@ -1,4 +1,28 @@
 let connections = [];
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+
+// Load the saved theme from local storage
+let currentThemeSetting = localStorage.getItem("theme") || "light";
+document.querySelector("html").setAttribute("data-theme", currentThemeSetting);
+if (currentThemeSetting === "dark") {
+  sun.classList.add("visible");
+} else {
+  moon.classList.add("visible");
+}
+
+
+document.getElementById("darkModeToggle").addEventListener("click", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+
+  localStorage.setItem("theme", newTheme);
+  currentThemeSetting = newTheme;
+
+  sun.classList.toggle("visible");
+  moon.classList.toggle("visible");
+});
 
 async function loadSavedConnections() {
   try {
